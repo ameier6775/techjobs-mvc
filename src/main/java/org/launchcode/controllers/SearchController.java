bf75e0fa396d04731d4473e72921a08002a4b97a
Alex Meier <ameier6775@gmail.com>
2019-06-14 21:11:55 -0500

    Finishing up second assignment

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by LaunchCode
  */
 @Controller
-@RequestMapping("search")
+@RequestMapping("/search")
 public class SearchController {
 
-    @RequestMapping(value = "")
+    @RequestMapping
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
         return "search";
@@ -24,4 +26,88 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value = "/results", params = {"searchType", "searchTerm"})
+    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        model.addAttribute("columns", ListController.columnChoices);
+
+        List<HashMap<String, String>> searchResults;
+
+        if (searchType.equalsIgnoreCase("all")) {
+            searchResults = JobData.findByValue(searchTerm);
+        }
+        else {
+            searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        List<SearchResult> results = addSearchResultsToModel(searchResults);
+
+        model.addAttribute("results", results);
+        model.addAttribute("size", results.size());
+
+        return "search";
+    }
+
+    private List<SearchResult> addSearchResultsToModel(List<HashMap<String, String>> searchResults)
+    {
+        return searchResults.stream().map(result -> {
+
+            SearchResult searchResultAdd = new SearchResult();
+
+            searchResultAdd.setName(result.get("name"));
+            searchResultAdd.setEmployerName(result.get("employer"));
+            searchResultAdd.setEmployerPositionType(result.get("position type"));
+            searchResultAdd.setEmployerLocation(result.get("location"));
+            searchResultAdd.setEmployerCoreCompetency(result.get("core competency"));
+            return searchResultAdd;
+        }).collect(Collectors.toList());
+    }
+
+    private class SearchResult{
+
+        private String name;
+        private String employerName;
+        private String employerPositionType;
+        private String employerLocation;
+        private String employerCoreCompetency;
+
+        public String getEmployerName() {
+            return employerName;
+        }
+
+        public void setEmployerName(String employerName) {
+            this.employerName = employerName;
+        }
+
+        public String getEmployerPositionType() {
+            return employerPositionType;
+        }
+
+        public void setEmployerPositionType(String employerPositionType) {
+            this.employerPositionType = employerPositionType;
+        }
+
+        public String getEmployerLocation() {
+            return employerLocation;
+        }
+
+        public void setEmployerLocation(String employerLocation) {
+            this.employerLocation = employerLocation;
+        }
+
+        public String getEmployerCoreCompetency() {
+            return employerCoreCompetency;
+        }
+
+        public void setEmployerCoreCompetency(String employerCoreCompetency) {
+            this.employerCoreCompetency = employerCoreCompetency;
+        }
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
